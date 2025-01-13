c061f3c2da5f41caa0c816287006377725276953
manwarcr <chrisman504@gmail.com>
2025-01-13 18:48:35 +0000

    second

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/manwarcr/puppy
+
+go 1.23.4
+
+require github.com/manwarcr/dog v0.0.0-20250109195553-1938603f7de8
---
+++ puppy.go
@@ -0,0 +1,19 @@
+package puppy
+
+import "github.com/manwarcr/dog"
+
+func Bark() string {
+	return "Woof!"
+}
+
+func Barks() string {
+	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
+}
